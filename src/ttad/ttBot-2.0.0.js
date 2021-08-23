bac0ad20326f74b6f118c7b3097808540a6ce542
Rafael Silva <rafael_c_105@hotmail.com>
2021-08-23 19:59:33 -0300

    a

--- src/ttad/ttBot-2.0.0.js
+++ src/ttad/ttBot-2.0.0.js
@@ -57,7 +57,7 @@ const getUrl = () => {
     puppeteer,
     puppeteerOptions: {
       headless: true,
-      executablePath: '/usr/bin/google-chrome-stable',
+      executablePath: '/usr/bin/google-chrome',
       // executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
       // executablePath: 'C:/Users/User/Desktop/Twitch/Chromium/bin/chrome.exe',
       // headless: true,
