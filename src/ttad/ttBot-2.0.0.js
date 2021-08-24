01b5f2ded532b94f4a1d418e63a559944e864bd8
Rafael Silva <rafael_c_105@hotmail.com>
2021-08-24 19:35:59 -0300

    a

--- src/ttad/ttBot-2.0.0.js
+++ src/ttad/ttBot-2.0.0.js
@@ -58,8 +58,7 @@ const getUrl = () => {
     puppeteer,
     puppeteerOptions: {
       headless: true,
-      executablePath: '/usr/bin/google-chrome-stable',
-      // executablePath: '/home/rafaeldecarvalho_ps/ungoogled-chromium_92.0.4515.159_1.vaapi_linux/chrome',
+      executablePath: '/home/rafaeldecarvalho_ps/ungoogled-chromium_92.0.4515.159_1.vaapi_linux/chrome',
       // executablePath: '/home/ubuntu/ungoogled-chromium_92.0.4515.159_1.vaapi_linux/chrome',
       // executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
       // executablePath: 'C:/Users/User/Desktop/Twitch/Chromium/bin/chrome.exe',
@@ -76,71 +75,58 @@ const getUrl = () => {
     await page.setUserAgent(userAgent);
 
     // Deal with twitch cookies and unnecessary contests
-    // const client = await page.target().createCDPSession();
+    const client = await page.target().createCDPSession();
 
-    // await client.send('Network.enable');
-    // await client.send('Network.setRequestInterception', {
-    //   patterns: [{
-    //     urlPattern: '*',
-    //   }],
-    // });
+    await client.send('Network.enable');
+    await client.send('Network.setRequestInterception', {
+      patterns: [{
+        urlPattern: '*',
+      }],
+    });
 
-    // client.on('Network.requestIntercepted', async ({
-    //   interceptionId,
-    //   request: httpRequest,
-    //   resourceType,
-    // }) => {
-    //   const continueParams = { interceptionId };
-    //   const requestUrl = httpRequest.url.split('?')[0].split('#')[0];
+    client.on('Network.requestIntercepted', async ({
+      interceptionId,
+      request: httpRequest,
+      resourceType,
+    }) => {
+      const continueParams = { interceptionId };
+      const requestUrl = httpRequest.url.split('?')[0].split('#')[0];
 
-    //   if (httpRequest.url === 'https://www.twitch.tv/') {
-    //     continueParams.rawResponse = 'eyBzdGF0dXM6IDIwMCwgY29udGVudFR5cGU6ICd0ZXh0L3BsYWluJywgYm9keTogJzxodG0nICB9';
-    //   } else if (
-    //     blockedResourceTypes.indexOf(resourceType) !== -1
-    //   || skippedResources.some((resource) => requestUrl.indexOf(resource) !== -1)
-    //   ) {
-    //     continueParams.errorReason = 'AddressUnreachable';
-    //   }
-    //   client.send('Network.continueInterceptedRequest', continueParams)
-    //     .catch(() => null);
-    // });
-    // // ///////////////////////////////////////
+      if (httpRequest.url === 'https://www.twitch.tv/') {
+        continueParams.rawResponse = 'eyBzdGF0dXM6IDIwMCwgY29udGVudFR5cGU6ICd0ZXh0L3BsYWluJywgYm9keTogJzxodG0nICB9';
+      } else if (
+        blockedResourceTypes.indexOf(resourceType) !== -1
+      || skippedResources.some((resource) => requestUrl.indexOf(resource) !== -1)
+      ) {
+        continueParams.errorReason = 'AddressUnreachable';
+      }
+      client.send('Network.continueInterceptedRequest', continueParams)
+        .catch(() => null);
+    });
+    // ///////////////////////////////////////
 
-    // // set cookies to mute audio and accept mature content
-    // await page.goto('https://www.twitch.tv/');
-    // await page.evaluate(() => {
-    //   localStorage.setItem('mature', 'true');
-    //   localStorage.setItem('video-muted', '{"default":false}');
-    //   localStorage.setItem('volume', '0.5');
-    //   localStorage.setItem('video-quality', '{"default":"160p30"}');
-    // });
+    // set cookies to mute audio and accept mature content
+    await page.goto('https://www.twitch.tv/');
+    await page.evaluate(() => {
+      localStorage.setItem('mature', 'true');
+      localStorage.setItem('video-muted', '{"default":false}');
+      localStorage.setItem('volume', '0.5');
+      localStorage.setItem('video-quality', '{"default":"160p30"}');
+    });
     // /////////////////////////////////////////////////
     await page.goto(uri);
-    await page.waitForSelector('[data-a-target="player-overlay-mature-accept"]')
-    await page.screenshot({ path: `screenshot-mature${views}.png` } )
-    await page.evaluate(() => {
-      document.querySelector('[data-a-target="player-overlay-mature-accept"]').click();
-    });
     console.log('Waiting for selector...')
-    await page.screenshot({ path: `screenshot-apos-goto${views}.png` } )
-    await page.waitForSelector('[data-a-target="video-player"]')
-    await page.screenshot({ path: `screenshot-apos-waitForSelector${views}.png` } )
-    console.log('Passou do waitForSelector')
-    await page.mouse.click(240, 50, { clickCount: 2 });
-    console.log('clicouuuu')
-    views += 1;
-    await page.screenshot({ path: `screenshot${views}.png` } )
-    console.log('tirou screenshot')
     await page.waitForSelector(AdSadOverlay, { timeout: 10000 });
-    console.log('Wainting selector disapear...')
+    console.log('Found selector...')
     await page.waitForSelector(AdSadOverlay, { hidden: true, timeout: 181000 });
+    console.log('Wainting selector disapear...')
 
     await sleep(
       getRandomIntInclusive(3000, 5000),
     );
 
     console.log('\nSUCCESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS !!!\n')
-    
+    views += 1;
 
     await page.close();
   });
